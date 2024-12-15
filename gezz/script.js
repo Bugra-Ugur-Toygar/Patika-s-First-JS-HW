bd52793843828c2263036193e20a7ec10cf75fe4
Buğra Uğur Toygar <151760316+Bugra-Ugur-Toygar@users.noreply.github.com>
2024-12-15 18:44:55 +0300

    Add files via upload

---
+++ gezz/script.js
@@ -0,0 +1,32 @@
+// Kullanıcı adını al ve sayfaya yerleştir
+let userName = prompt("Lütfen adınızı girin:");
+document.getElementById("myName").textContent = userName ? userName : "Misafir";
+
+// Saati dakikayı ve saniyeyi göstermek içim
+function showTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const days = [
+    "Pazar",
+    "Pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+  ];
+  const day = days[now.getDay()];
+
+  document.getElementById(
+    "myClock"
+  ).textContent = `${hours}:${minutes}:${seconds} ${day}`;
+}
+
+// setInterval belirtilen süre boyunca fonksiyonu tekrarlı olarak çalıstırır.
+setInterval(showTime, 1000);
+
+// Anlık olarak saat ve tarihi göstermek için
+
+showTime();
